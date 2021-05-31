b566227e07cf6d12e30a045e4cd73176083b7c64
Nuocheng Shen <shennu@oregonstate.edu>
2021-05-30 18:29:31 -0700

    add reverse two word test case

--- test_reverses.py
+++ test_reverses.py
@@ -9,6 +9,9 @@ class testReverse(unittest.TestCase):
     def test_single(self):
         self.assertEqual(reverses.reverse("abc"), "abc")
 
+    def test_two_words(self):
+        self.assertEqual(reverses.reverse("Hello World"), "World Hello")
+
 
 if __name__ == "__main__":
     unittest.main()
