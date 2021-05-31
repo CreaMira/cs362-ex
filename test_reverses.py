bd46d8bd9e340e698d8288d59bcc7920c3689911
Nuocheng Shen <shennu@oregonstate.edu>
2021-05-30 18:23:28 -0700

    test case 2
    but not need code

--- test_reverses.py
+++ test_reverses.py
@@ -5,6 +5,9 @@ import reverses
 class testReverse(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(reverses.reverse(""), "")
+    
+    def test_single(self):
+        self.assertEqual(reverses.reverse("abc"), "abc")
 
 
 if __name__ == "__main__":
